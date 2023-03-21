ae6b606aa9ce3c90e70a2a0c8e09eb50fbecba57
Mubarek Seid Juhar <mubareksd@gmail.com>
2023-03-21 18:12:44 +0300

    solution to task 6, project 0x01-es6_promise

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,12 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]).then((response) => response.map((item) => ({
+      status: item.status,
+      value: item.value === 'fulfilled' ? item.value : String(item.reason),
+    })));
+}
