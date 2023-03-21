6e53dd1ca18e87281e712278f6bc2b7326ca977f
Mubarek Seid Juhar <mubareksd@gmail.com>
2023-03-21 19:38:15 +0300

    update

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,6 +1,6 @@
 export default function divideFunction(numerator, denominator) {
   if (denominator === 0) {
-    return Error('Can not divide by zero');
+    return Error('cannot divide by 0');
   }
   return (numerator / denominator);
 }
