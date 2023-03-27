2d0c583a04f9d3e2f04fba6549fe6171ec14298a
Mubarek Seid Juhar <mubareksd@gmail.com>
2023-03-27 21:55:23 +0300

    update

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,7 +2,7 @@ const cleanSet = (set, startString) => {
   const arr = [];
   if (startString && typeof startString === 'string') {
     for (const item of set) {
-      if (item.startsWith(startString)) arr.push(item.slice(startString.length));
+      if (item && item.startsWith(startString)) arr.push(item.slice(startString.length));
     }
   }
   return arr.join('-');
