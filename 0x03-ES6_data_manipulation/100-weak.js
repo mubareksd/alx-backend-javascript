2bfc0a692e6362738fd21e28eb5e6f2e0491d648
Mubarek Seid Juhar <mubareksd@gmail.com>
2023-03-27 23:39:55 +0300

    update

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,10 +1,10 @@
 const weakMap = new WeakMap();
 let count = 0;
 
-const queryAPI = (endpoint) => {
-  weakMap.set(endpoint, count);
+const queryAPI = (endPoint) => {
+  weakMap.set(endPoint, count);
   count += 1;
-  if (weakMap.get(endpoint) >= 5) {
+  if (weakMap.get(endPoint) >= 5) {
     throw Error('Endpoint load is high');
   }
 };
