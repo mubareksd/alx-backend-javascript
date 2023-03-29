06cc850591022d643f2a05698ad8a477fe07fb94
Mubarek Seid Juhar <mubareksd@gmail.com>
2023-03-29 15:13:39 +0300

    solution to task 4, project 0x04-typescript

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -11,4 +11,28 @@ interface Directors extends Teacher {
     numberOfReports: number
 }
 
-const  printTeacher = (firstName: string, lastName: string) => `${firstName.charAt(0)}. ${lastName}`;
+interface printTeacherFunction {
+    (firstName: string, lastname: string): string;
+}
+
+const  printTeacher: printTeacherFunction = (firstName: string, lastName: string) : string => `${firstName.charAt(0)}. ${lastName}`;
+
+interface StudentInterface {
+    firstName: string,
+    lastName: string,
+    workOnHomework(): string,
+    displayName(): string
+}
+
+class StudentClass implements StudentInterface {
+    firstName: string;
+    lastName: string;
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    workOnHomework = () : string => 'Currently working';
+
+    displayName = () : string => this.firstName;
+}
