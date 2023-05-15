a3c000b674c493d9150bd5fce6fe4eb963fef624
Mubarek Seid Juhar <mubareksd@gmail.com>
2023-05-15 18:57:03 +0300

    task 1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,8 +1,12 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
-process.stdin.on('data', (data) => {
-  process.stdout.write(`Your name is: ${data}`);
+
+process.stdin.on('readable', () => {
+  const data = process.stdin.read();
+  if (data !== null) {
+    process.stdout.write(`Your name is: ${data}`);
+  }
 });
 
-process.on('exit', () => {
-  console.log('This important software is now closing\n');
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
