451c4e2ee3835e05233b95c7414610f2492e8ee9
Mubarek Seid Juhar <mubareksd@gmail.com>
2023-05-15 20:02:18 +0300

    task 4

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,9 +1,13 @@
 const http = require('http');
 
 const app = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello Holberton School!');
 });
 
 app.listen(1245, '0.0.0.0', () => {
   console.log('Listening on port 1245');
 });
+
+
+module.exports = app;
