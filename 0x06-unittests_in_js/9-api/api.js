eb72042125d89dc3e2994b6dbc49cc5b28d34cc2
Mubarek Seid Juhar <mubareksd@gmail.com>
2023-05-18 14:42:14 +0300

    update

--- 0x06-unittests_in_js/9-api/api.js
+++ 0x06-unittests_in_js/9-api/api.js
@@ -3,11 +3,11 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.status(200).send('Welcome to the payment system');
+  res.send('Welcome to the payment system');
 });
 
 app.get('/cart/:id([0-9]+)', (req, res) => {
-  res.status(200).send(`Payment methods for cart ${req.params.id}`);
+  res.send(`Payment methods for cart ${req.params.id}`);
 });
 
 app.listen(7865, '0.0.0.0', () => { 
